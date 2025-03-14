6dcfb6db9c8e471587a32cb404ec4fb3747d21f7
mba emmanuel <Emmanuelmba329@gmail.com>
2025-03-14 10:19:45 +0100

    Deploy to GitHub Pages

---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const viewAll = () => {
+  return (
+    <div className='bg-red-400 rounded-lg p-5 mx-10'>viewAll</div>
+  )
+}
+
+export default viewAll
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,12 @@
+import Hero from '../components/Hero';
+import React from 'react'
+
+const Homepage = () => {
+  return (
+    <>  <Hero title="Become a Better React dev" subtitle="Find the react Job that suits you"/> </>
+    
+ 
+  )
+}
+
+export default Homepage
