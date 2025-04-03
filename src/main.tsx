d240c228a0a7565cf3668e250eea817f5ca711e2
mba emmanuel <Emmanuelmba329@gmail.com>
2025-04-03 22:22:58 +0100

    some chat gpt stuff

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,17 @@ import { createRoot } from 'react-dom/client'
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query'
 import './index.css'
 import App from './App.jsx'
+import "@radix-ui/themes/styles.css";
 
-createRoot(document.getElementById('root')).render(
- 
-  <StrictMode>
-     <QueryClientProvider client={new QueryClient()}>
-   
-      <App />
-  </QueryClientProvider>,
-  </StrictMode>,
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <QueryClientProvider client={new QueryClient()}>
+        <App />
+      </QueryClientProvider>
+    </StrictMode>,
+  );
+} else {
+  console.error("Root element not found");
+}
